e48e5baafef46eefd18c07cf617d7b7d74db0a2a
Josh <j.adam.downes@gmail.com>
2014-12-21 17:48:46 -0500

    Added basic button in javafx

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 //2%
 
-public class Main {
+//Added imports for Application parent class, staging, scenes, and a button class
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 
+//Class extends applicatoin
+public class Main extends Application{
+        
+        //@Override // start method in application class
+        public void start(Stage primaryStage){
+            //Create a scene and place the button in the scene
+            Scene twoPercent = new Scene(new Button("2%"),250,250);
+            primaryStage.setTitle("2%");
+            primaryStage.setScene(twoPercent); //Sets scene in stage
+            primaryStage.show();//Display stage
+        }
+        
 	public static void main(String args[]) {
-		new Main();
+		launch(args);//Launch arguments
 	}
 
 	public Main() {
